8c9e72ccd0167038bf28ae82a0809be3d5dc735f
gabikohen <gabokohen@gmail.com>
2022-07-16 20:38:07 -0300

    practice assigments part1

---
+++ fundamentals/assignments.js
@@ -0,0 +1,37 @@
+// JavaScript Fundamentals – Part 1
+
+/* LECTURE: Values and Variables*/
+
+const country = "Portugal";
+const continent = "Europe";
+let population = 11;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/* LECTURE: Data Types &  LECTURE: let, const and var  */
+
+let isIsland = false;
+let lenguage = "Portuguse";
+
+console.log(isIsland, population, country, lenguage);
+
+/*LECTURE: Basic Operators*/
+
+let populationHalf = 11 / 2 / 2;
+console.log(populationHalf);
+
+let increase = 11 + 1;
+console.log(increase);
+
+// Mi contry has more that Findland
+
+// No, My country has less than 33M
+
+let description =
+  "Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(description);
+
+/*LECTURE: Strings and Template Literals*/
+
--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,3 +1,33 @@
 let js = 'amazing';
-if(js === "amazing") alert("javascript is FUN!")
-console.log(40 +50 +44 -66);
+/* if(js === "amazing") alert("javascript is FUN!")*/
+console.log(40 +50 +44 -66); 
+
+
+// Value is of a pieace of data
+// Vlaue is small unit of information
+
+ console.log('Gabriel'); 
+
+console.log(23);
+
+// Type the name of variables
+
+let firstName = "Pirata";
+let first = "Panchuk";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(first);
+console.log(firstName);
+console.log(firstName);
+
+const PI = 3.1413;
+
+
+let myFirstJob = 'machine learnings';
+let myCurrentJob = 'Teacher';
+
+
+
+
