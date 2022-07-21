071940efbe17fa933a9e2a5c6571faca3245d8cc
gabikohen <gabokohen@gmail.com>
2022-07-20 22:20:41 -0300

    data types

--- fundamentals/assignments.js
+++ fundamentals/assignments.js
@@ -1,7 +1,9 @@
+
+
 // JavaScript Fundamentals – Part 1
 
 /* LECTURE: Values and Variables*/
-
+/*
 const country = "Portugal";
 const continent = "Europe";
 let population = 11;
@@ -9,29 +11,32 @@ let population = 11;
 console.log(country);
 console.log(continent);
 console.log(population);
-
+*/
 /* LECTURE: Data Types &  LECTURE: let, const and var  */
-
+/*
 let isIsland = false;
+    isIsland = true;
 let lenguage = "Portuguse";
 
 console.log(isIsland, population, country, lenguage);
 
 /*LECTURE: Basic Operators*/
 
-let populationHalf = 11 / 2 / 2;
+/* let populationHalf = 11 / 2 / 2;
 console.log(populationHalf);
 
 let increase = 11 + 1;
 console.log(increase);
-
+ */
 // Mi contry has more that Findland
 
 // No, My country has less than 33M
 
-let description =
+/* let description =
   "Portugal is in Europe, and its 11 million people speak portuguese";
 console.log(description);
+ */
+/*LECTURE: Strings and Template Literals*/ 
 
-/*LECTURE: Strings and Template Literals*/
+
 
--- fundamentals/script.js
+++ fundamentals/script.js
@@ -1,5 +1,6 @@
-let js = 'amazing';
+/* let js = 'amazing';
 /* if(js === "amazing") alert("javascript is FUN!")*/
+/*
 console.log(40 +50 +44 -66); 
 
 
@@ -28,6 +29,51 @@ const PI = 3.1413;
 let myFirstJob = 'machine learnings';
 let myCurrentJob = 'Teacher';
 
+// Type of Data
+
+// Number used for decimals and integers
+  let age = 34;
+
+  // String used for text
+
+let firstNames = 'Gabriel';
+
+// Logical type can be true o false
+
+let fullAge = true;
+
+// Undefined cases no have value 
+
+let panchuk;
+*/
+// Sybol  value unique and cannot be changed  
 
 
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+//is soperatio just the minus operator that we can use to show the type of a value
+
+
+
+/* console.log(typeof true); */
+console.log(typeof false);
+
+console.log(typeof 23);
+console.log(typeof 'hola');
+
+// reasing value 
+javascriptIsFun = 'Yes!'
+
+console.log(typeof javascriptIsFun);
+
+let number;
+
+number = 25
+console.log(number)
+
+// this bug javascript 
+console.log(typeof null)
 
